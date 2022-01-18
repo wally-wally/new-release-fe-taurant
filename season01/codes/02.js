5d2ed22dd84f56612c79198cdfe697ea7f3098f7
kwonmory <kwonmory@gmail.com>
2022-01-18 23:27:18 +0900

    number 2 sovled

--- season01/codes/02.js
+++ season01/codes/02.js
@@ -1,7 +1,19 @@
 export function question01(words) {
   // TODO: 여기에 함수 내부 로직을 작성해주세요.
+
+  return !!words.find(isHaveSpecialCharacter);
+
+  function isHaveSpecialCharacter(word) {
+    return /[&?;:]/g.test(word);
+  }
 }
 
 export function question02(words) {
   // TODO: 여기에 함수 내부 로직을 작성해주세요.
+
+  return words.map(replaceAmpersandToHtmlSpecialCharacterCode);
+
+  function replaceAmpersandToHtmlSpecialCharacterCode(word) {
+    return word.replace(/&/g, '&amp;');
+  }
 }
