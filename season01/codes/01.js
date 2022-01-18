1c111bf0354751a3da067a52ce0ae3ee72194d68
kwonmory <kwonmory@gmail.com>
2022-01-18 23:06:40 +0900

    number 1 sovled

--- season01/codes/01.js
+++ season01/codes/01.js
@@ -1,3 +1,6 @@
 export function checkAvailableRegisterReceiveRule(mailboxId) {
   // TODO: 여기에 함수 내부 로직을 작성해주세요.
+  const availableRegisterTargets = [1, 2, 4, 5];
+  
+  return availableRegisterTargets.includes(mailboxId) ? 'available' : 'unavailable';
 }
